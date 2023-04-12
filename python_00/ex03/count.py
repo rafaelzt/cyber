6444177da0436796bced728f375d67c075c36698
Rafael Z Tanganelli <kabelu@gmail.com>
2023-04-12 11:38:04 +0200

    trying to solve number of arguments

--- python_00/ex03/count.py
+++ python_00/ex03/count.py
@@ -6,54 +6,65 @@
 #    By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/11 17:23:21 by rzamolo-          #+#    #+#              #
-#    Updated: 2023/04/11 19:25:45 by rzamolo-         ###   ########.fr        #
+#    Updated: 2023/04/12 11:32:24 by rzamolo-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
 punctuation = [',', '.', ':', ';', '!', '?', '"'
-				, "'", '-', '(', ')', '[', ']', '{', '}']
+                , "'", '-', '(', ')', '[', ']', '{', '}']
 
 try:
-	def text_analyzer(argument = sys.argv[1:]):
-		'''
-		This function counts the number of upper characters, lower characters,
-		punctuation and spaces in a given text.
-		'''
-		punct = 0
-		upper = 0
-		lower = 0
-		space = 0
+    # print("Before function")
+    # print(sys.argv)
+    def text_analyzer(argument = sys.argv):
+        '''
+        This function counts the number of upper characters, lower characters,
+        punctuation and spaces in a given text.
+        '''
+        punct = 0
+        upper = 0
+        lower = 0
+        space = 0
 
-		if not(isinstance(argument, str)):
-			print("The argument is not a string.")
-			return
+        # print(argument)
 
-		if len(argument) == 0:
-			print("What is the text to analyse?")
-			argument = input()
-		for letter in argument:
-			if letter.isupper():
-				upper += 1
-			elif letter.islower():
-				lower += 1
-			elif letter.isspace():
-				space += 1
-			elif letter in punctuation:
-				punct += 1
-			
-		print("The text contains", len(argument), "character(s):")
-		print("-", upper, "upper letter(s)")
-		print("-", lower, "lower letter(s)")
-		print("-", punct, "punctuation mark(s)")
-		print("-", space, "space(s)")
+        if (len(argument) > 2):
+            print("You should enter only one argument.")
+        elif len(argument) == 1:
+            print("What is the text to analyse?")
+            argument = input()
+        else:
+            argument = ''.join(argument[1:])
+
+        if not(isinstance(argument, str)):
+            print("The argument is not a string.")
+            return
+
+        for letter in argument:
+            if letter.isupper():
+                upper += 1
+            elif letter.islower():
+                lower += 1
+            elif letter.isspace():
+                space += 1
+            elif letter in punctuation:
+                punct += 1
+            
+        print("The text contains", len(argument), "character(s):")
+        print("-", upper, "upper letter(s)")
+        print("-", lower, "lower letter(s)")
+        print("-", punct, "punctuation mark(s)")
+        print("-", space, "space(s)")
+        # print(sys.argv) No exist
+        print(argument)
 except TypeError as te:
-	print("You should enter only one argument.")
-		
-	
+    print("You should enter only one argument.")
+
+
+        
+    
 if __name__ == "__main__":
-	if (len(sys.argv) > 2):
-		print("You should enter only one argument.")
-	else:
-		text_analyzer(''.join(sys.argv[1:]))
+    # print(len(sys.argv))
+    text_analyzer(''.join(sys.argv[1:]))
