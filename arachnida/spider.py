0c9f14567988a68706efc33c7eb8f639f2785f56
Rafael Z Tanganelli <kabelu@gmail.com>
2023-04-18 13:29:26 +0200

    wip

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -6,13 +6,12 @@
 #    By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/17 12:45:24 by rzamolo-          #+#    #+#              #
-#    Updated: 2023/04/18 10:53:38 by rzamolo-         ###   ########.fr        #
+#    Updated: 2023/04/18 12:53:59 by rzamolo-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import argparse
 import os
-import sys
 
 import requests
 from bs4 import BeautifulSoup
@@ -24,8 +23,7 @@ from bs4 import BeautifulSoup
 # print("Text: {}\n".format(r.text))
 # print("Json: {}\n".format(r.json))
 
-def ft_get_all_links(url) -> list:
-    domain = "https://" + url
+def ft_get_all_links(domain) -> list:
     r = requests.get(domain)
     page_source = r.text
     soup = BeautifulSoup(page_source, 'html.parser')
@@ -36,12 +34,14 @@ def ft_get_all_links(url) -> list:
     # Search all <a> tags
     for link in soup.find_all('a'):
         links = link.get('href')
-        if (url in links):
+        if (domain in links) and not(domain in full_domain_lst):
             full_domain_lst.append(links)
         else:
             continue
 
     full_domain_lst = list(dict.fromkeys(full_domain_lst)) # Remove duplicated URLs
+#    for link in full_domain_lst:
+#        ft_get_all_links(link)
     return(full_domain_lst)
 
 def ft_get_all_images(url_lst, path):
@@ -59,11 +59,11 @@ def ft_get_all_images(url_lst, path):
                 # with open(path + filename, 'wb') as file:
                 # 	for chunk in r.iter_content(chunk_size=1024):
                 # 		file.write(chunk)
-                with open(path + filename, 'wb') as file:
+                with open(path + '/' + filename, 'wb+') as file:
                         file.write(r.content)
                 
     # with open(file_name, 'wb') as f:
-    #     for chunk in get_response.iter_content(chunk_size=1024):
+    #     for chunk in r.iter_content(chunk_size=1024):
     #         if chunk: # filter out keep-alive new chunks
     #             f.write(chunk)
 
@@ -76,23 +76,27 @@ def ft_get_all_images(url_lst, path):
 #     print("Images: {images}".format(images = link.get('src')))
 
 if __name__ == "__main__":
-    # try:
-    # 	path = ("./" + sys.argv[2] + "/")
-    # 	os.mkdir(path)
-    # except FileExistsError as fe:
-    # 	print("WARN:\n\tFolder already exist!\n\t{}".format(fe))
-    # ft_get_all_images(ft_get_all_links(sys.argv[1]), path)
-
     parser = argparse.ArgumentParser(description='Webscrap an URL and download images')
-    parser.add_argument('URL', required=True,
+    # Positional arguments
+    parser.add_argument("url", 
                         help='Specify URL to scrap')
-    parser.add_argument('Recursive', metavar='-r', nargs=1, required=False,
-                        help='Recursivly download images')
-    parser.add_argument('Depth', metavar='-l [N]', type=int, nargs=1, required=False,
+    # Optional arguments
+    parser.add_argument("-r", "--recursive", dest='r',
+                        help='Recursivly download images', action='store_true')
+    parser.add_argument("-l", "--length", metavar='[N]', dest='l',
                         help='Depth you want to scrap', default=5)
-    parser.add_argument('Path' ,metavar='-p [PATH]', type=str, nargs=1, required=False,
+    parser.add_argument("-p", "--path", metavar='[PATH]', dest="p", 
                         help='Specify were downloaded images should be saved', default='./data/')
 
     args = parser.parse_args()
-    print(args.URL)
+    print(args.r)
+    print(args.l)
+    try:
+        os.mkdir(args.p)
+    except FileExistsError as fe:
+        print("WARN:\n\tFolder already exist!\n\t{}".format(fe))
+    links = ft_get_all_links(args.url)
+    for item in links:
+        print(item)
+    # ft_get_all_images(links, args.p)
     
