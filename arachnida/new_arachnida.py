06ac8066217abe80546670cd7437a284b10dbaaf
Rafael Z Tanganelli <kabelu@gmail.com>
2023-04-19 10:32:32 +0200

    creating recursive and multilvel scrape

--- arachnida/new_arachnida.py
+++ arachnida/new_arachnida.py
@@ -6,20 +6,21 @@
 #    By: rzamolo- <rzamolo-@student.42madrid.com>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/18 15:06:57 by rzamolo-          #+#    #+#              #
-#    Updated: 2023/04/18 17:53:25 by rzamolo-         ###   ########.fr        #
+#    Updated: 2023/04/19 10:30:48 by rzamolo-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 
 import argparse
-import exifread
-import sys
+# import exifread
 import os
 import re
-from requests.models import HTTPError
+import sys
 import urllib3
 
+
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
 def ft_parse_arguments(*arg):
@@ -78,19 +79,35 @@ def ft_warmup_my_soup(content):
 
 def ft_extract_link_from_soup(soup):
     links = []
-    for link in (soup.findAll('a', attrs={'href': re.compile("^https://")})):
+    for link in (soup.findAll('a', attrs={'href': re.compile("^https?://")})):
         links.append(link.get('href'))
     return (links)
 
+def ft_recursive_list(scrape_lst, original_url):
+    if len(scrape_lst) == 0:
+        return
+    print(scrape_lst[0])
+    ft_recursive_list(scrape_lst[1:], original_url)
 
-def iterate_list(lst):
+def ft_create_multi_level_lst(url, iter = 0):
+    domain = urlparse(url).netloc
+    lst = []
+    while (iter >= 0):
+        print("Domain: {} -> URL: {}".format(domain, url))
+        if (domain in url):
+            lst.append(ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(url))))
+        iter -= 1
+    return (iter + 1, lst)
+
+
+#def iterate_list(lst):
     # base case: if the list is empty, return
-    if len(lst) == 0:
-       return
+#    if len(lst) == 0:
+#       return
     
     # recursive case: iterate into the first element of the list
-    print(lst[-1])
-    iterate_list(lst[:-1])
+#    print(lst[-1])
+#    iterate_list(lst[:-1])
 
 if __name__ == "__main__":
     #arguments = ft_parse_arguments(sys.argv)
@@ -100,20 +117,12 @@ if __name__ == "__main__":
     #print("Depth: \t\t\t{} \t\t\t type: {}".format(arguments.depth, type(arguments.depth)))
     #print("Path: \t\t\t{} \t\t\t type: {}".format(arguments.path, type(arguments.path)))
 
-    iterate_list(range(5))
+    #iterate_list(range(5))
     #my_list = [1,2,3,4,5]
     #print(my_list)
     #print(my_list[::-1])
 
-    nivel_0 = (ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content("https://42madrid.com"))))
-    print(nivel_0)
-    nivel_0_1 = ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(nivel_0[0])))
-    print(nivel_0_1)
-    nivel_0_2 = ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(nivel_0[1])))
-    print(nivel_0_2)
-    nivel_0_3 = ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(nivel_0[2])))
-    print(nivel_0_3)
-    nivel_0_4 = ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(nivel_0[3])))
-    print(nivel_0_4)
-    nivel_0_5 = ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(nivel_0[4])))
-    print(nivel_0_5)
+    url = "https://42madrid.com"
+
+    levels = ft_create_multi_level_lst(url)
+    print(levels)
