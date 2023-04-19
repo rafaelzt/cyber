451976b601e95b4a498881d771f51c6d5bdc0b59
Rafael Z Tanganelli <kabelu@gmail.com>
2023-04-19 17:25:59 +0200

    not working

--- arachnida/new_arachnida.py
+++ arachnida/new_arachnida.py
@@ -6,7 +6,7 @@
 #    By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/18 15:06:57 by rzamolo-          #+#    #+#              #
-#    Updated: 2023/04/19 13:30:18 by rzamolo-         ###   ########.fr        #
+#    Updated: 2023/04/19 17:22:52 by rzamolo-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -90,52 +90,60 @@ def ft_extract_link_from_soup(soup):
     return (links)
 
 
-def ft_recursive_list(scrape_lst, original_url):
+def ft_create_url_lst(scrape_lst, original_url):
     global lst_items
+    
     domain = urlparse(original_url).netloc
+    
     if len(scrape_lst) == 0: 
         return
 
-    if (domain in scrape_lst[0]):
-        #print("Domain: {} \t->\t Scrape: {}".format(domain, scrape_lst[0]))
+    if (scrape_lst[0][0] == '/'):
+        scrape_lst[0] = domain + '/' + scrape_lst[0][0]
+
+    if (domain in scrape_lst[0]) and not(scrape_lst[0] in lst_items):
+        # print("Domain: {} \t->\t Scrape: {}".format(domain, scrape_lst[0], len(scrape_lst)))
+        if (scrape_lst[0][-1] != '/'):
+            scrape_lst[0] = scrape_lst[0] + '/'
         lst_items.append(scrape_lst[0])
-        lst_items = list(dict.fromkeys(lst_items))
-
-    ft_recursive_list(scrape_lst[1:], original_url)
+        
 
 
-def ft_create_multi_level_lst(lst_items, iteration = 0):
+    ft_create_url_lst(scrape_lst[1:], original_url)
+
+
+def ft_create_multi_level_lst(url, iteration = 0):
     global num_calls
-
-    if not(num_calls >= iteration):
-        for item in lst_items:
-            print(item, num_calls)
-            ft_create_multi_level_lst(lst_items[])
-        num_calls += 1
-    else:
-        return
+    
+    ft_create_url_lst(ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(url))), url)
+    
 
 
 if __name__ == "__main__":
-    #arguments = ft_parse_arguments(sys.argv)
-    #print("Program: \t\t{} \t type: {}".format(sys.argv[0], type(sys.argv[0])))
-    #print("URL: \t\t\t{} \t\t type: {}".format(arguments.url, type(arguments.url)))
-    #print("Recursive Flag: \t{} \t\t\t type: {}".format(arguments.recursive, type(arguments.recursive)))
-    #print("Depth: \t\t\t{} \t\t\t type: {}".format(arguments.depth, type(arguments.depth)))
-    #print("Path: \t\t\t{} \t\t\t type: {}".format(arguments.path, type(arguments.path)))
+    # arguments = ft_parse_arguments(sys.argv)
+    # print("Program: \t\t{} \t type: {}".format(sys.argv[0], type(sys.argv[0])))
+    # print("URL: \t\t\t{} \t\t type: {}".format(arguments.url, type(arguments.url)))
+    # print("Recursive Flag: \t{} \t\t\t type: {}".format(arguments.recursive, type(arguments.recursive)))
+    # print("Depth: \t\t\t{} \t\t\t type: {}".format(arguments.depth, type(arguments.depth)))
+    # print("Path: \t\t\t{} \t\t\t type: {}".format(arguments.path, type(arguments.path)))
 
-    #iterate_list(range(5))
-    #my_list = [1,2,3,4,5]
-    #print(my_list)
-    #print(my_list[::-1])
+    # iterate_list(range(5))
+    # my_list = [1,2,3,4,5]
+    # print(my_list)
+    # print(my_list[::-1])
 
-    url = "https://42madrid.com"
-    domain = urlparse(url).netloc
-    #print(ft_get_content(url))
-    #print(ft_warmup_my_soup(ft_get_content(url)))
-    lst = ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(url)))
-    ft_recursive_list(lst, url)
+    # url = "https://42madrid.com"
+    # print(ft_get_content(url))
+    # print(ft_warmup_my_soup(ft_get_content(url)))
+    # lst = ft_extract_link_from_soup(ft_warmup_my_soup(ft_get_content(url)))
+    # ft_recursive_list(lst, url)
     # ft_create_multi_level_lst(url)
     # print(lst_items)
 
-    ft_create_multi_level_lst(lst, 1)
+    # ft_create_multi_level_lst(lst, 1)
+    ft_create_multi_level_lst("42madrid.com", 2)
+    print(lst_items)
+
+    # lst_items.pop(0) 
+    # del  lst_items[0]
+    # lst_items = lst_items[1:]
