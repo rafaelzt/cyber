fa6f28edeec7297999d67343ba112dd0363c1999
Rafael Z Tanganelli <kabelu@gmail.com>
2023-05-04 21:47:38 +0200

    before pull

--- coRSAir/ft_corsair.h
+++ coRSAir/ft_corsair.h
@@ -6,7 +6,15 @@
 /*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 11:44:38 by rzamolo-          #+#    #+#             */
-/*   Updated: 2023/04/24 11:44:39 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2023/04/24 17:54:01 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_CORSAIR_H
+# define FT_CORSAIR_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
