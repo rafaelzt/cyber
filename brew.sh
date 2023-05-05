a61893b7cd2e4e4abff3efd0c26c219eb0693392
Rafael Z Tanganelli <kabelu@gmail.com>
2023-05-05 15:47:00 +0200

    trying to read .pem file

---
+++ brew.sh
@@ -0,0 +1,6 @@
+rm -rf $HOME/goinfre/.brew && git clone https://github.com/Homebrew/brew $HOME/goinfre/.brew
+#echo -e 'export PATH=$HOME/goinfre/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc
+brew update
+brew install openssl@1.1
+
+
--- coRSAir/corsair.h
+++ coRSAir/corsair.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_corsair.h                                       :+:      :+:    :+:   */
+/*   corsair.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 11:44:38 by rzamolo-          #+#    #+#             */
-/*   Updated: 2023/04/24 17:54:01 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2023/05/05 12:40:09 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,27 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+
+# include <math.h>
+# include <string.h>
+
+# include <openssl/aes.h>
+# include <openssl/bio.h>
+# include <openssl/bn.h>
+
+# include <openssl/crypto.h>
+# include <openssl/err.h>
+# include <openssl/evp.h>
+
+# include <openssl/pem.h>
+# include <openssl/pkcs12.h>
+# include <openssl/rsa.h>
+
+# include <openssl/ssl.h>
+# include <openssl/x509.h>
+
+
+# define BuffSize 1024
 
 #endif
